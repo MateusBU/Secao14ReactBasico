a9e998999678a7344871e117c932d9b1fb2150d8
Mateus Urtassum <mateusurtassum@gmail.com>
2022-03-12 12:39:31 -0300

    https://github.com/luizomf/curso-js/tree/master/REACT/listatarefas

--- listatarefa/src/components/Form/index.js
+++ listatarefa/src/components/Form/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Form.css';
-// FORM
+// FORMs
 import { FaPlus } from 'react-icons/fa';
 
 // eslint-disable-next-line react/prop-types
